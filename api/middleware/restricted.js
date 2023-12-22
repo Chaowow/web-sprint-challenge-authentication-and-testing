b2e72755d0881300495d89c7450b995ac96a6f16
Frankie <frankiejosefej@gmail.com>
2023-12-22 01:20:41 -0500

    met mvp

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = require('../../configs');
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+  const token = req.headers.authorization;
 
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({
+          message: 'token invalid'
+        });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(403).json({
+      message: 'token required'
+    });
+  }
 };
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,74 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+const request = require('supertest');
+const db = require('../data/dbConfig');
+const server = require('./server');
+
+const user1 = { username: 'Reptile', password: 'AcidSpit'};
+const user2 = { username: 'Sub-Zero', password: 'IceKlone' };
+
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db('users').truncate();
+});
+afterAll(async () => {
+  await db.destroy();
+});
+
+describe('[POST] /api/auth/register', () => {
+  it('returns 201 OK', async () => {
+    const res = await request(server).post('/api/auth/register').send(user1);
+      expect(res.status).toBe(201);
+  });
+  it('registers new user and returns new user', async () => {
+    const res = await request(server).post('/api/auth/register').send(user2);
+      expect(res.status).toBe(201);
+      expect(res.body.username).toBe('Sub-Zero');
+  });
+});
+
+describe('[POST] /api/auth/login', () => {
+  it('registers then logins and then returns 200 OK', async () => {
+    await request(server).post('/api/auth/register').send(user1);
+    const res = await request(server).post('/api/auth/login').send(user1);
+      expect(res.status).toBe(200);
+  });
+  it('registers then logins and gives back token', async () => {
+    await request(server).post('/api/auth/register').send(user2);
+    const res = await request(server).post('/api/auth/login').send(user2);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('token');
+  });
+});
+
+describe('[GET] /api/jokes/', () => {
+  it('registers, logins, and gets then returns 200 OK', async () => {
+    await request(server).post('/api/auth/register').send(user1);
+
+    const loginResponse = await request(server).post('/api/auth/login').send(user1);
+    const token = loginResponse.body.token;
+
+    const res = await request(server)
+    .get('/api/jokes')
+    .set('Authorization', `${token}`);
+
+    expect(res.status).toBe(200);
+  });
+
+  it('registers, logins, and gets then returns data', async () => {
+    await request(server).post('/api/auth/register').send(user2);
+
+    const loginResponse = await request(server).post('/api/auth/login').send(user2);
+    const token = loginResponse.body.token;
+
+    const res = await request(server)
+    .get('/api/jokes')
+    .set('Authorization', `${token}`);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(3);
+  });
+});
